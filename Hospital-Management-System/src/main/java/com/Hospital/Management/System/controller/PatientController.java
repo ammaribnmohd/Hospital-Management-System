6dd8e7933ace7a48620d650b5a8de38d42b936ab
Ammar Mohammad <ammaribnmohammad@gmail.com>
2025-07-16 04:09:55 +0600

    Initial commit with frontend-hospitalms and Hospital-Management-System

---
+++ Hospital-Management-System/src/main/java/com/Hospital/Management/System/controller/PatientController.java
@@ -0,0 +1,38 @@
+package com.Hospital.Management.System.controller;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Hospital.Management.System.entity.Patient;
+import com.Hospital.Management.System.repository.PatientRepository;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/v1")
+public class PatientController {
+	
+	
+	PatientRepository patientRepository;
+
+	public PatientController(PatientRepository patientRepository) {
+		super();
+		this.patientRepository = patientRepository;
+	}
+	
+	@PostMapping("/insert")
+	public Patient createPatient(@RequestBody Patient patient) {
+		return patientRepository.save(patient);
+		
+	}
+	
+	@GetMapping
+	public List<Patient> getAllPatients() {
+		return patientRepository.findAll();
+	}
+	
+}
